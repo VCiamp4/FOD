800895b371c0780982c968c8491c358222002f73
Valentin Ciamparella <66568788+VCiamp4@users.noreply.github.com>
2023-04-19 12:03:27 -0300

    Add files via upload

--- program ejercicio7;.pas
+++ program ejercicio7;.pas
@@ -1,91 +1,156 @@
 program ejercicio7;
-const
-    valoralto = 9999;
+{Se cuenta con un archivo que almacena información sobre especies de aves en
+vía de extinción, para ello se almacena: código, nombre de la especie, familia de ave,
+descripción y zona geográfica. El archivo no está ordenado por ningún criterio. Realice
+un programa que elimine especies de aves, para ello se recibe por teclado las especies a
+eliminar. Deberá realizar todas las declaraciones necesarias, implementar todos los
+procedimientos que requiera y una alternativa para borrar los registros. Para ello deberá
+implementar dos procedimientos, uno que marque los registros a borrar y posteriormente
+otro procedimiento que compacte el archivo, quitando los registros marcados. Para
+quitar los registros se deberá copiar el último registro del archivo en la posición del registro
+a borrar y luego eliminar del archivo el último registro de forma tal de evitar registros
+duplicados.
+Nota: Las bajas deben finalizar al recibir el código 500000}
+const valoralto = 9999;
 type
-    producto = record
-        code:integer;
-        nom:string;
-        precio:real;
-        act:integer;
-        minimo:integer;
+    ave = record
+        code: integer;
+        especie: string;
+        familia:string;
+        desc:string;
+        zona:string;
     end;
 
-    venta = record
-        code:integer;
-        cant:integer;
-    end;begin
-        
+    archivo = file of ave;
+
+    lista := ^nodo;
+
+    nodo = record
+        dato: string;
+        sig: lista;
     end;
 
-    detalle = file of venta;
-    archivo = file of producto;
-
-procedure leer(var d:detalle;var v:venta);
+procedure cargar_archivo(var a:archivo);
+var
+    v:ave;
 begin
-    if (not eof(d)) then
-        read(d,v);
+    v.code := 0;
+    while (v.code > -1) then begin
+        writeln('--Ingrese codigo: ');read(v.code);
+        writeln('--Especie: ');read(v.especie);
+        writeln('--Familia: ');read(v.familia);
+        writeln('--Descripcion: ');read(v.desc);
+        writeln('--zona: ');read(v.zona);
+        write(a,v);
+    end;
+    writeln('Archivo creado!');
+end;
+
+
+procedure leer(var a:archivio;var v:ave);
+begin
+    if (not eof(a)) then
+        read(a,v);
     else
-        v.code := valoralto;
+        v.code:= valoralto;
 end;
-    
-procedure cargar(var a:archivo;var d:detalle;var v:venta);
+
+procedure lista_codigos(var l:lista;c:integer);
 var
-    cont:integer;
-    actual:integer;
-    am: producto;
+    aux: lista;
 begin
-    leer(d,v)
-    while (v.code <> valoralto) do begin
-            cant := 0
-            act := v.code;
-            while (act = v.code) do begin
-                cant := cant + v.cant;
-                leer(d,v);  //creo que con tener el leer aca ya es suficiente. Chequear;
-            end;  //hasta aca se hace un merge del detalle
-            while ((not eof(a))and(am.code <> act)) do begin
-                read(a,am);
+    new(aux)
+    aux^.dato := c;
+    aux^.sig := l;
+    l:=aux;
+end;
+
+procedure marcar (var a:archivo;l:lista);
+var
+    aux:lista;
+    v:ave;
+begin
+    while (l^.sig := null) do begin
+        //vuelvo al principio del archivo
+        leer(a,v);
+        while (v.code <> valoralto) do begin
+            if (v.especio = aux^.dato) then begin
+                v.code := -v.code;
+                seek(a,filepos(a)-1);
+                write(a,v);
             end;
-            if (am.code = act) then begin
-                am.act := am.act - cant;
-                seek(a,filepos(a)-); //?? creo que no es necesario porque nunca termino de saltar a otra posicion;
-                write(a,am);
-            end;
+            leer(a,v);
+        seek(a,0);
+        l := l^.sig;
     end;
+    writeln('Elementos marcados con exito!');
 end;
 
-procedure tex();
-var ma:archivo;texto:text;p:productos;
+procedure borrar (var a:archivo);
+var
+    v:ave;pos:integer;aux:ave;
 begin
-  assign(ma,'maestro');
-  reset(ma);
-  assign(texto,'stock_minimo.txt');
-  rewrite(texto);
-  while (not(eof(ma))) do begin
-    read(ma,p);
-    if(p.stockA<p.stockM) then begin
-      with p do begin
-        writeln(texto,code,' ',precio:0:2,' ',nom);
-        writeln(texto,act,' ',minimo);
-      end;
-    end;  
-  end;
-  close(texto);
+    while (not eof(a)) do begin
+        leer(a,v);
+        if (v.code < 0) then begin
+            if (eof(a)) then begin
+                seek(a,filepos(a)-1);
+                truncate(a);
+            end
+            else begin
+                pos := filepos(a)-1;
+                seek(a,filesize(a)-1);
+                read(a,aux);
+                seek(a,filepos(a)-1);
+                truncate(a);
+                seek(a,pos);
+                write(a,aux);
+                seek(a,filepos(a)-1);
+            end;
+        end;
+    end;
+    writeln('elementos borrados con exito!');
+end;
+
+procedure listar (var a:archivo);
+var
+    aux:ave;
+    texto:text;
+begin
+    assign(texto,'aves.txt');
+    rewrite(texto);
+    leer(a,aux);
+    while (aux.code <> valoralto) then begin
+        write(texto,aux.code,'',aux.especie);
+        write(texto,aux.familia);
+        write(texto,aux.desc);
+        leer(a,aux);
+    end;
+    close(texto);
 end;
 
 var
-    m:archivo;
-    d:detalle;
-    v:venta;
+    a:archivo;
+    l:lista;
+    st:integer;
 begin
-    assign(m,'maestro');
-    assign(d,'detalle');
+    assign(a,'archivo_de_aves');
+    rewrite(a);
 
-    reset(m);
-    reset(d);
+    cargar_archivo(a);
 
-    cargar(a,d,v);
-    tex();
+    writeln('----Ingrese especies de aves a borrar----');
+    read(st);
+    while (st <> 50000) do begin
+        lista_codigos(l,st);
+        write('Ingrese codigo de especie a borrar: ');
+        read(st);
+    end;
 
-    close(m);
-    close(d);
+
+    marcar(a,l);
+    borrar(a);
+    listar(a);
+
+    close(a);
 end.
